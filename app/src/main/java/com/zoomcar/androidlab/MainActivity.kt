bee66916fec3b1146706b0fb187374bf66549600
Gideon Paul <gideon.paul@zoomcar.com>
2023-01-04 14:33:49 +0530

    Working model.

--- app/src/main/java/com/zoomcar/androidlab/MainActivity.kt
+++ app/src/main/java/com/zoomcar/androidlab/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.zoomcar.androidlab
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.zoomcar.zuikit.dimensions.ZDimension
+import com.zoomcar.zuikit.label.ZLabel
+import com.zoomcar.zuikit.typography.ZTypography
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Hello()
+        }
     }
+}
+
+@Composable
+fun Hello() {
+    Column(modifier = Modifier.fillMaxSize().padding(ZDimension.D16.dp)) {
+        ZLabel(text = "Hello Zoomer", style = ZTypography.H3)
+        ZLabel(text = "Experiment Away", style = ZTypography.BODY2)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewHello() {
+    Hello()
 }
