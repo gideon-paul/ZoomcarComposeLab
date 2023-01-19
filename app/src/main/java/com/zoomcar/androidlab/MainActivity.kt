412017d91e2c3d86b9da286a71e4598d694382fb
Gideon Paul <gideon.paul@zoomcar.com>
2023-01-19 18:35:11 +0530

    Base setup.

--- app/src/main/java/com/zoomcar/androidlab/MainActivity.kt
+++ app/src/main/java/com/zoomcar/androidlab/MainActivity.kt
@@ -3,12 +3,14 @@ package com.zoomcar.androidlab
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.zoomcar.zuikit.button.ZButton
 import com.zoomcar.zuikit.dimensions.ZDimension
 import com.zoomcar.zuikit.label.ZLabel
 import com.zoomcar.zuikit.typography.ZTypography
@@ -17,21 +19,88 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Hello()
+            SocialMediaIntentsTester()
         }
     }
 }
 
 @Composable
-fun Hello() {
-    Column(modifier = Modifier.fillMaxSize().padding(ZDimension.D16.dp)) {
-        ZLabel(text = "Hello Zoomer", style = ZTypography.H3)
-        ZLabel(text = "Experiment Away", style = ZTypography.BODY2)
+fun SocialMediaIntentsTester() {
+    Column(
+        modifier = Modifier
+        .fillMaxSize()
+        .padding(all = ZDimension.D16.dp),
+        verticalArrangement = Arrangement.spacedBy(ZDimension.D16.dp)
+    ) {
+        ZLabel(
+            text = "Social Media Intents",
+            style = ZTypography.H3
+        )
+
+        ZButton(
+            text= "Facebook",
+            fullWidth = true,
+            onClick = {
+
+            }
+        )
+
+        ZButton(
+            text= "Messenger",
+            fullWidth = true,
+            onClick = {
+
+            }
+        )
+
+        ZButton(
+            text= "Viber",
+            fullWidth = true,
+            onClick = {
+
+            }
+        )
+
+        ZButton(
+            text= "Twitter",
+            fullWidth = true,
+            onClick = {
+
+            }
+        )
+
+        ZButton(
+            text= "Zelo",
+            fullWidth = true,
+            onClick = {
+
+            }
+        )
     }
+}
+
+fun handleFacebook(message: String) {
+
+}
+
+fun handleMessenger(message: String) {
+
+}
+
+fun handleViber(message: String) {
+
+}
+
+fun handleTwitter(message: String) {
+
+}
+
+fun handleZelo(message: String) {
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun PreviewHello() {
-    Hello()
+fun PreviewSocialMediaIntentsTester() {
+    SocialMediaIntentsTester()
 }
