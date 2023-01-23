013449e50ef83ee60c7b5574a867b5f816992561
Gideon Paul <gideon.paul@zoomcar.com>
2023-01-23 15:37:55 +0530

    Whatsapp is done.

--- app/src/main/java/com/zoomcar/androidlab/MainActivity.kt
+++ app/src/main/java/com/zoomcar/androidlab/MainActivity.kt
@@ -36,7 +36,7 @@ fun SocialMediaIntentsTester() {
 
     val title = "My message"
     val message =
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel magna vel erat tincidunt tincidunt at fringilla leo. Nam sit amet magna eget orci pellentesque rhoncus. Cras pellentesque, tellus vitae gravida sodales, dolor enim euismod sem, nec dignissim arcu nibh ac libero. Cras tincidunt magna nec arcu condimentum, ut vehicula diam feugiat. Integer ac maximus ante, vitae cursus velit. Praesent viverra faucibus dictum. Aliquam quis nisi tortor. Phasellus ut auctor leo, id aliquam odio. "
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel magna vel erat tincidunt tincidunt at fringilla leo. Nam sit amet magna eget orci pellentesque rhoncus. Cras pellentesque, tellus vitae gravida sodales, dolor enim euismod sem, nec dignissim arcu nibh ac libero. Cras tincidunt magna nec arcu condimentum, ut vehicula diam feugiat. Integer ac maximus ante, vitae cursus velit. Praesent viverra faucibus dictum. Aliquam quis nisi tortor. Phasellus ut auctor leo, id aliquam odio. https://www.zoomcar.com/"
 
     Column(
         modifier = Modifier
@@ -47,6 +47,14 @@ fun SocialMediaIntentsTester() {
         ZLabel(
             text = "Social Media Links",
             style = ZTypography.H3
+        )
+
+        ZButton(
+            text = "Whatsapp",
+            fullWidth = true,
+            onClick = {
+                handleWhatsapp(context, "917483785428", message)
+            }
         )
 
         ZButton(
@@ -82,29 +90,51 @@ fun SocialMediaIntentsTester() {
         )
 
         ZButton(
-            text = "Zelo",
+            text = "Zalo",
             fullWidth = true,
             onClick = {
-
+                handleZello(context, message)
             }
         )
     }
 }
 
+/**
+ * Send message to a phone number on Whatsapp. It appears that the phone number need not be saved
+ * in the user contacts for this to work.
+ *
+ * @param context Context object.
+ * @param phoneNumber Must be a valid phone number with country code. The number can be preceded by
+ * a plus sign. though that's not a requirement.
+ * @param message Message to be sent to the user.
+ */
+fun handleWhatsapp(context: Context, phoneNumber: String, message: String) {
+    val uri = Uri.parse("whatsapp://send?phone=$phoneNumber&text=$message")
+    val intent = Intent(Intent.ACTION_VIEW, uri)
+    intent.putExtra(Intent.EXTRA_TEXT, message)
+    context.startActivity(intent)
+}
+
+/**
+ * Looks like Facebook allows only sharing an url, not text.
+ *
+ * See https://stackoverflow.com/a/14496338/3376001
+ *
+ */
 fun handleFacebook(context: Context, message: String) {
     try {
         val intent = Intent()
         intent.apply {
             `package` = "com.facebook.katana"
-            action = "android.intent.action.SEND"
+            action = Intent.ACTION_SEND
             type = "text/plain"
-            putExtra("android.intent.extra.TEXT", message)
+            putExtra(Intent.EXTRA_TEXT, message)
             context.startActivity(intent)
         }
     } catch (e: Exception) {
-        val sharerUrl = "https://www.facebook.com/sharer/sharer.php?u=$message"
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(sharerUrl))
-        context.startActivity(intent)
+//        val sharerUrl = "https://www.facebook.com/sharer/sharer.php?u=$message"
+//        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(sharerUrl))
+//        context.startActivity(intent)
     }
 }
 
@@ -155,8 +185,20 @@ fun handleTwitter(context: Context, message: String) {
     }
 }
 
-fun handleZelo(message: String) {
+fun handleZello(context: Context, message: String) {
+    val intent = Intent()
+    intent.apply {
+        `package` = "com.twitter.android"
+        action = Intent.ACTION_SEND
+        putExtra(Intent.EXTRA_TEXT, message)
+        type = "text/plain"
+    }
 
+    try {
+        context.startActivity(intent)
+    } catch (ex: ActivityNotFoundException) {
+        // TODO: Handle failure
+    }
 }
 
 @Preview(showBackground = true)
